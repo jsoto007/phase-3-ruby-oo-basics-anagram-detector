455ace769594bacee7cc947dee49026bf92586dc
Jose Soto <jsoto7087@gmail.com>
2023-01-18 22:49:40 -0500

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+
+class Anagram
+        def initialize(word)
+            @word = word
+        end 
+
+        def match(match_arr)
+            match_arr.select { |word| word.split("").sort == @word.split("").sort}
+        end
+end 
+
+
