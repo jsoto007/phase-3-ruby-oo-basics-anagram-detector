a6648d649e3b08ab8a536090477a9117dbe72bfe
Jose Soto <jsoto7087@gmail.com>
2023-02-25 21:04:30 -0500

    commit tree 9fdc50b139672262370f87f62b7c5e89e0cdbc8c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,39 @@
 # Your code goes here!
 
 
-class Anagram
-        def initialize(word)
-            @word = word
-        end 
 
-        def match(match_arr)
-            match_arr.select { |word| word.split("").sort == @word.split("").sort}
-        end
+class Anagram
+
+  def initialize(word)
+    @word = word
+
+  end 
+
+  def match(match_arr)
+    match_arr.select { |word| word.split("").sort == @word.split("").sort}
+  end
+
+
 end 
+
+
+
+
+
+
+
+
+
+
+
+# class Anagram
+#         def initialize(word)
+#             @word = word
+#         end 
+
+#         def match(match_arr)
+#             match_arr.select { |word| word.split("").sort == @word.split("").sort}
+#         end
+# end 
 
 
